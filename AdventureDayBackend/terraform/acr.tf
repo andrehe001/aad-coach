5d3cba0f8173300531c301827d27e4496bb59358
Oliver Lohmann <oliver@lohmann.io>
2020-09-08 21:19:51 +0200

    Use the right pull secret setup for ACR-AKS.

--- AdventureDayBackend/terraform/acr.tf
+++ AdventureDayBackend/terraform/acr.tf
@@ -9,5 +9,6 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_role_assignment" "acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  skip_service_principal_aad_check = true
 }
