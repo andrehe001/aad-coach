04ade1049367095df4e9019082fdbd702bc26750
Ricardo Niepel <ricardo.niepel@outlook.com>
2020-09-09 23:03:01 +0200

    adding auth #4

--- AdventureDayBackend/frontend/src/router/index.js
+++ AdventureDayBackend/frontend/src/router/index.js
@@ -3,15 +3,24 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const defaultRequiresAuth = true;
+
+const routes = [
   {
     path: '/',
     name: 'Home',
     redirect: '/leaderboard'
   },
   {
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'main', guest: true },
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
     path: '/code-of-conduct',
     name: 'Code of Conduct',
+    meta: { layout: 'main' },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -40,11 +49,53 @@ Vue.use(VueRouter)
     name: 'Your Accounts',
     meta: { layout: 'main' },
     component: () => import(/* webpackChunkName: "team-accounts" */ '../views/TeamAccounts.vue')
+  },
+  {
+    path: '/administration',
+    name: 'Administration',
+    meta: { layout: 'main', is_admin: true },
+    component: () => import(/* webpackChunkName: "administration" */ '../views/Administration.vue')
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.path == '/logout') {
+    localStorage.clear();
+    next({ path: '/'});
+    router.go();
+  } else if(to.matched.some(record => record.meta.requiresAuth) || (defaultRequiresAuth && !to.matched.some(record => record.meta.guest))) {
+      if (localStorage.getItem('jwt') == null) {
+          next({
+              path: '/login',
+              params: { nextUrl: to.fullPath }
+          })
+      } else {
+          let user = JSON.parse(localStorage.getItem('user'))
+          if(to.matched.some(record => record.meta.is_admin)) {
+              if(user.is_admin == 1){
+                  next()
+              }
+              else{
+                  next({ path: '/leaderboard'})
+              }
+          }else {
+              next()
+          }
+      }
+  } else if(to.matched.some(record => record.meta.guest)) {
+      if(localStorage.getItem('jwt') == null){
+          next()
+      }
+      else{
+          next({ path: '/leaderboard'})
+      }
+  }else {
+      next()
+  }
 })
 
 export default router
