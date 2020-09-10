e50b617f9ad89e95663b1e00194c049a4b45cbc5
Ricardo Niepel <ricardo.niepel@outlook.com>
2020-09-10 16:13:43 +0200

    move portal-api and add some notes to API spec

---
+++ AdventureDayBackend/portal-api/src/team-management-data/ITeamManagement.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace team_management_data
+{
+    public interface ITeamManagement
+    {
+        // JwtToken (teamId, username and IsAdmin Flag) Login(Username, Pwd)
+
+
+        // APIs require a valid jwt token
+        Task<ActionResult<IEnumerable<Member>>> GetTeamAccounts(Guid id);
+
+        Task<ActionResult<Team>> UpdateTeamName(Guid id, string name);
+
+        Task<ActionResult<Team>> GetTeamStatsAndLogs(Guid id);
+
+        Task<ActionResult<IEnumerable<Team>>> GetStats();
+
+        // APIs require a valid admin jwt token
+        Task<ActionResult<Team>> UpdateTeamNameAdmin(Guid id, string name);
+
+        Task<ActionResult<IEnumerable<Member>>> GetTeamAccountsAdmin(Guid id);
+    }
+}
