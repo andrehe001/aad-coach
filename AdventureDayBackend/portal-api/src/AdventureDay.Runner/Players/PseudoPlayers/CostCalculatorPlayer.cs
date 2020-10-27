66c94af4341827f6841d70909813c2cf52691cea
Ricardo Niepel <ricardo.niepel@outlook.com>
2020-10-27 16:55:40 +0100

    add cost player exception handler, fix #55

--- AdventureDayBackend/portal-api/src/AdventureDay.Runner/Players/PseudoPlayers/CostCalculatorPlayer.cs
+++ AdventureDayBackend/portal-api/src/AdventureDay.Runner/Players/PseudoPlayers/CostCalculatorPlayer.cs
@@ -43,7 +43,7 @@ namespace AdventureDay.Runner.Players.PseudoPlayers
         public CostCalculatorPlayer(IConfiguration configuration, Team team, TimeSpan httpTimeout) : base(configuration, team, httpTimeout)
         {
             _configuration = configuration;
-           _azureCostScaleFactor = _configuration.GetValue("AzureCostScaleFactor", 1); 
+            _azureCostScaleFactor = _configuration.GetValue("AzureCostScaleFactor", 1);
         }
 
         public override string Name => "Dagobert";
@@ -72,70 +72,79 @@ namespace AdventureDay.Runner.Players.PseudoPlayers
         private async Task<long> GetSqlCostAsync(IAzure azure)
         {
             long sqlCost = 0;
-            var sqlCostScaleFactor = 265;
 
-            foreach (var sqlServer in await azure.SqlServers.ListAsync())
+            try
             {
-                foreach (var database in await sqlServer.Databases.ListAsync())
+                var sqlCostScaleFactor = 265;
+
+                foreach (var sqlServer in await azure.SqlServers.ListAsync())
                 {
-                    // Azure Policy ensure only GeneralPurpose is allowed
-                    var serviceLevel = database.ServiceLevelObjective.ToString(); // GP_S_Gen5_1 etc.
-                    if (serviceLevel.StartsWith("GP_S_Gen5"))
+                    foreach (var database in await sqlServer.Databases.ListAsync())
                     {
-                        var appCpuBilledMetric = (await azure.MetricDefinitions.ListByResourceAsync(database.Id))
-                            .SingleOrDefault(_ => _.Name.Value == "app_cpu_billed");
+                        // Azure Policy ensure only GeneralPurpose is allowed
+                        var serviceLevel = database.ServiceLevelObjective.ToString(); // GP_S_Gen5_1 etc.
+                        if (serviceLevel.StartsWith("GP_S_Gen5"))
+                        {
+                            var appCpuBilledMetric = (await azure.MetricDefinitions.ListByResourceAsync(database.Id))
+                                .SingleOrDefault(_ => _.Name.Value == "app_cpu_billed");
 
-                        var now = DateTime.Now.ToUniversalTime();
+                            var now = DateTime.Now.ToUniversalTime();
 
-                        var metricCollection = await appCpuBilledMetric
-                                .DefineQuery()
-                                .StartingFrom(now.AddMinutes(-5))
-                                .EndsBefore(now)
-                                .WithInterval(TimeSpan.FromMinutes(1))
-                                .ExecuteAsync();
+                            var metricCollection = await appCpuBilledMetric
+                                    .DefineQuery()
+                                    .StartingFrom(now.AddMinutes(-5))
+                                    .EndsBefore(now)
+                                    .WithInterval(TimeSpan.FromMinutes(1))
+                                    .ExecuteAsync();
 
-                        // https://docs.microsoft.com/en-us/azure/azure-sql/database/serverless-tier-overview#metrics
-                        // Unit: vCore seconds
-                        var lastMinuteAppCPUBilled = metricCollection.Metrics
-                            .Single()
-                            .Timeseries
-                            .SelectMany(_ => _.Data)
-                            .OrderByDescending(_ => _.TimeStamp)
-                            .First()
-                            .Total;
+                            // https://docs.microsoft.com/en-us/azure/azure-sql/database/serverless-tier-overview#metrics
+                            // Unit: vCore seconds
+                            var lastMinuteAppCPUBilled = metricCollection.Metrics
+                                .Single()
+                                .Timeseries
+                                .SelectMany(_ => _.Data)
+                                .OrderByDescending(_ => _.TimeStamp)
+                                .First()
+                                .Total;
 
-                        var test = metricCollection.Metrics
-                            .Single()
-                            .Timeseries
-                            .SelectMany(_ => _.Data)
-                            .OrderByDescending(_ => _.TimeStamp)
-                            .ToList();
+                            var test = metricCollection.Metrics
+                                .Single()
+                                .Timeseries
+                                .SelectMany(_ => _.Data)
+                                .OrderByDescending(_ => _.TimeStamp)
+                                .ToList();
 
-                        // COMPUTE COST / VCORE / SECOND: 0.000134 EUR
-                        var costPerVCorePerSecond = 0.000134;
-                        var usedVCoreSecondsInLastMinute = lastMinuteAppCPUBilled.GetValueOrDefault(); // max 60 * vCoreCount
+                            // COMPUTE COST / VCORE / SECOND: 0.000134 EUR
+                            var costPerVCorePerSecond = 0.000134;
+                            var usedVCoreSecondsInLastMinute = lastMinuteAppCPUBilled.GetValueOrDefault(); // max 60 * vCoreCount
 
-                        // 1 VCore ganze Minute
-                        //var cost = costPerVCorePerSecond * 60 * 265;
-                        //2,1306
-                        sqlCost += (int)Math.Round(costPerVCorePerSecond * usedVCoreSecondsInLastMinute * sqlCostScaleFactor, MidpointRounding.AwayFromZero);
-                    }
-                    else if (serviceLevel.StartsWith("GP_Gen5"))
-                    {
-                        var vCoreCount = int.Parse(serviceLevel.Replace("GP_Gen5_", ""));
+                            // 1 VCore ganze Minute
+                            //var cost = costPerVCorePerSecond * 60 * 265;
+                            //2,1306
+                            sqlCost += (int)Math.Round(costPerVCorePerSecond * usedVCoreSecondsInLastMinute * sqlCostScaleFactor, MidpointRounding.AwayFromZero);
+                        }
+                        else if (serviceLevel.StartsWith("GP_Gen5"))
+                        {
+                            var vCoreCount = int.Parse(serviceLevel.Replace("GP_Gen5_", ""));
 
-                        // Cost per vCore (in EUR, month) 167.77
-                        var costPerVCorePerSecond = 0.000063;
-                        var usedVCoreSecondsInLastMinute = 60 * vCoreCount;
+                            // Cost per vCore (in EUR, month) 167.77
+                            var costPerVCorePerSecond = 0.000063;
+                            var usedVCoreSecondsInLastMinute = 60 * vCoreCount;
 
-                        // 1 VCore ganze Minute
-                        //var cost = costPerVCorePerSecond * 60 * 265;
-                        //1,0017
+                            // 1 VCore ganze Minute
+                            //var cost = costPerVCorePerSecond * 60 * 265;
+                            //1,0017
 
-                        // * scaleFactor just for having bigger numbers
-                        sqlCost += (int)Math.Round(costPerVCorePerSecond * usedVCoreSecondsInLastMinute * sqlCostScaleFactor, MidpointRounding.AwayFromZero);
+                            // * scaleFactor just for having bigger numbers
+                            sqlCost += (int)Math.Round(costPerVCorePerSecond * usedVCoreSecondsInLastMinute * sqlCostScaleFactor, MidpointRounding.AwayFromZero);
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "CostCalculatorPlayer: GetSqlCostAsync had an issue");
+                sqlCost += 1;
             }
 
             return sqlCost;
