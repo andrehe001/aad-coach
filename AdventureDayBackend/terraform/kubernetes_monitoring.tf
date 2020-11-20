e3e1778a4a27c70f06104a31c45838a23b2ee8c0
Oliver Lohmann <oliver@lohmann.io>
2020-11-20 08:36:05 +0100

    Prometheus Operator was renamed to Kube-Prometheus-Stack

--- AdventureDayBackend/terraform/kubernetes_monitoring.tf
+++ AdventureDayBackend/terraform/kubernetes_monitoring.tf
@@ -10,8 +10,8 @@ resource "kubernetes_namespace" "monitoring" {
 
 resource "helm_release" "prometheus_operator" {
   name      = "po"
-  chart     = "prometheus-operator"
-  repository = "https://kubernetes-charts.storage.googleapis.com"
+  chart     = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
   namespace = kubernetes_namespace.monitoring.metadata[0].name
 
   depends_on = [helm_release.nginx_ingress]
