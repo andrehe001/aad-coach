a0cdece25641b41bffb93afcc6351edf0d467241
Ricardo Niepel <ricardo.niepel@outlook.com>
2021-02-24 22:55:56 +0100

    enable K8s related props in AppInsights

--- TeamGameHub/GameEngine/WebApi/Startup.cs
+++ TeamGameHub/GameEngine/WebApi/Startup.cs
@@ -44,7 +44,7 @@ namespace AzureGameDay.Web
             }); 
 
             services.AddApplicationInsightsTelemetry();
-
+            services.AddApplicationInsightsKubernetesEnricher();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
