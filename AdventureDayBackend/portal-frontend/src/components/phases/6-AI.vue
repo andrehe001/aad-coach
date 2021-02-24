4c06f7f545837336ca874d8fc6838ecb82abb205
Ricardo Niepel <ricardo.niepel@outlook.com>
2021-02-24 11:29:59 +0100

    remove ML links add useful stuff #86

--- AdventureDayBackend/portal-frontend/src/components/phases/6-AI.vue
+++ AdventureDayBackend/portal-frontend/src/components/phases/6-AI.vue
@@ -4,8 +4,15 @@
       <img src="../../assets/images/phases/6-AI_small.jpg" />
     </div>
     <h2>Phase 6: Intelligence</h2>
-    <p>Unfortunately, we must realize that the Smoorghs still win too often and we can't buy back enough resources. Can you optimize our artificial players in the system so that we win more often?</p>
-    <p>Goal: Optimize your artificial player bots and increase our change of winning!</p>
+    <p>
+      Unfortunately, we must realize that the Smoorghs still win too often and
+      we can't buy back enough resources. Can you optimize our artificial
+      players in the system so that we win more often?
+    </p>
+    <p>
+      Goal: Optimize your artificial player bots and increase our change of
+      winning!
+    </p>
     <h3>Hints</h3>
     <p>
       <a
@@ -14,12 +21,26 @@
         aria-expanded="false"
         aria-controls="collapseHints"
         href="#"
-      >Toggle Hints</a>
+        >Toggle Hints</a
+      >
     </p>
     <ul class="collapse" id="collapseHints">
-      <li>Even people sometimes have firmly established patterns</li>
-      <li>Are Smoorghs human-like?</li>
-      <li>The cloud has enough computing capacity for any algorithm or machine learning approach</li>
+      <li>
+        Even people sometimes have firmly established patterns,
+        <br />
+        Are Smoorghs human-like?
+      </li>
+      <li>
+        If you need some specific knowledge you don't have at your team (e.g.
+        SQL queries) think about alternative approaches!
+      </li>
+      <li>
+        How can you deploy a custom GameBot? Was already something prepared for you?
+      </li>
+      <li>
+        The cloud has enough computing capacity for any algorithm or machine
+        learning approach
+      </li>
     </ul>
     <h3>Helpful Resources</h3>
     <p>
@@ -29,33 +50,33 @@
         aria-expanded="false"
         aria-controls="collapseResources"
         href="#"
-      >Toggle Resources</a>
+        >Toggle Resources</a
+      >
     </p>
     <ul class="collapse" id="collapseResources">
       <li>
         <a
           href="https://docs.microsoft.com/en-us/azure/azure-sql/database/connect-excel"
-        >Connect Excel to a database in Azure SQL Database</a>
+          >Connect Excel to a database in Azure SQL Database</a
+        >
       </li>
       <li>
         <a
           href="https://docs.microsoft.com/en-us/power-bi/connect-data/service-azure-sql-database-with-direct-connect"
-        >Connect PowerBI Desktop to a database in Azure SQL Database</a>
+          >Connect PowerBI Desktop to a database in Azure SQL Database</a
+        >
       </li>
       <li>
         <a
-          href="https://docs.microsoft.com/en-us/azure/machine-learning/how-to-access-data"
-        >Connect to Azure SQL Database in Azure Machine Learning</a>
+          href="https://docs.microsoft.com/en-us/power-bi/connect-data/service-azure-sql-database-with-direct-connect"
+          >Use GitHub Actions to automate, customize, and execute your software development workflows</a
+        >
       </li>
       <li>
         <a
-          href="https://towardsdatascience.com/building-a-rock-paper-scissors-ai-using-tensorflow-and-opencv-d5fc44fc8222"
-        >Building a Rock Paper Scissors AI using Tensorflow and OpenCV with Python</a>
-      </li>
-      <li>
-        <a
-          href="https://towardsdatascience.com/how-to-win-over-70-matches-in-rock-paper-scissors-3e17e67e0dab"
-        >How to win over 70% matches in Rock Paper Scissors</a>
+          href="https://docs.microsoft.com/en-us/azure/container-registry/container-registry-tutorial-quick-task#build-in-azure-with-acr-tasks"
+          >Build and deploy container images in the cloud with Azure Container Registry Tasks</a
+        >
       </li>
     </ul>
   </div>
