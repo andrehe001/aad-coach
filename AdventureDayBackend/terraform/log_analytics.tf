e915dffa80a3bc59bc277427e5fc1f45735aad45
Dennis Zielke <11569044+denniszielke@users.noreply.github.com>
2021-09-20 17:30:03 +0200

    Update log_analytics.tf

--- AdventureDayBackend/terraform/log_analytics.tf
+++ AdventureDayBackend/terraform/log_analytics.tf
@@ -2,8 +2,7 @@ resource "azurerm_log_analytics_workspace" "la" {
   name                = "${local.prefix_kebab}-la-${local.hash_suffix}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
-  retention_in_days   = 30
+  sku                 = "PerGB2018"
 }
 
 
