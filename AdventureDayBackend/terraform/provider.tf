9d59051a31d3d0e3c495a12736066a913cee1610
Ricardo Niepel <ricardo.niepel@outlook.com>
2021-10-14 14:48:29 +0200

    add provider pinning #135

--- AdventureDayBackend/terraform/provider.tf
+++ AdventureDayBackend/terraform/provider.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.80.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "=2.5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "=2.3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
