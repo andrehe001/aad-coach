d87051323ea04afab12e21ea56ba0319f6e6606c
Ricardo Niepel <ricardo.niepel@outlook.com>
2021-10-14 10:12:06 +0200

    adding json-parser-test

---
+++ .github/actions/json-parser-test/index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const jsonToTest = core.getInput('json-data');
+    console.log(`Testing:`);
+    console.log(jsonToTest);
+
+    try {
+        this.dom = JSON.parse(jsonToTest);
+
+        console.log(`JSON parsing successful`);
+
+        const payload = JSON.stringify(dom, undefined, 2)
+        console.log(`The payload: ${payload}`);
+    }
+    catch (ex) {
+        core.setFailed(`Content is not a valid JSON object ${ex}`);
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
